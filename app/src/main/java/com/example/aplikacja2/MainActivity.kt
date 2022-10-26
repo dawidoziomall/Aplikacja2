a2eac92d233e14df158488f7ce7508fbc64ec337
student-14 <terleckawika123@gmail.com>
2022-10-26 11:13:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aplikacja2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.aplikacja2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.aplikacja2.R.id.button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+            findViewById<Button>(R.id.button).text = "Pies"
+
+        }
+
+    }
+}
